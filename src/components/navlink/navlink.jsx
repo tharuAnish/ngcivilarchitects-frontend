dead3d43d7f12e33e705f1ef4a0cb19a3575cd8e
tharuAnish <namoanishtharu@gmail.com>
2023-06-11 22:42:50 +0530

    navlink path name fixed

--- src/components/navlink/navlink.jsx
+++ src/components/navlink/navlink.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom"
-import styles from "./navLink.module.css"
+import styles from "./navlink.module.css"
 import classNames from "classnames"
 
 export default function NavLink({ link, content }) {
