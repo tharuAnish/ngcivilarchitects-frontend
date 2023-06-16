6ef49a905ab0d9c23ed4a93ac1c1fe2b5cb00fc0
tharuAnish <namoanishtharu@gmail.com>
2023-06-16 22:29:25 +0530

    project initials added

---
+++ src/pages/about/about.jsx
@@ -0,0 +1,10 @@
+import Nav from "../../components/nav/nav"
+
+export default function About() {
+  return (
+    <div>
+      <Nav />
+      <h1>ABOUT</h1>
+    </div>
+  )
+}
--- src/pages/services/services.jsx
+++ src/pages/services/services.jsx
@@ -5,6 +5,7 @@ import Header from "../../components/pageHeader/header"
 import s from "./services.module.css"
 import { useFetch } from "../../hooks/useFetch"
 import Footer from "../../components/footer/footer"
+import Tagline from "../../components/tagline/tagline"
 
 export default function Services() {
   const url = "http://127.0.0.1:8000/api/services/"
@@ -19,8 +20,17 @@ export default function Services() {
         headerP={"We provide the best construction services"}
       />
       <div className={`${s.servicesContainner} wrapper`}>
-        <h2>OUR SERVICES</h2>
-        <p>Explore our services</p>
+        <p className={s.titleP}>OUR SERVICES</p>
+        <h2 className={s.titleH2}>Explore Our Services</h2>
+        <p className={s.servicePara}>
+          At NG-Group, we take pride in providing an extensive range of
+          construction services with skilled professionals and tailored
+          solutions. From residential to commercial projects, we handle every
+          aspect of construction.
+          {/* Whether you're looking to build your dream
+          home or transform your commercial space, trust NG-Group for top-notch
+          construction services that exceed expectations. */}
+        </p>
         <div className={s.services}>
           {isPending && <p>Loading...</p>}
           {error && <p>{error}</p>}
@@ -44,6 +54,11 @@ export default function Services() {
         </div>
       </div>
       <Testimony />
+      <Tagline
+        tagH4="We Are Provinding Best Construction Services"
+        tagP="Crafting Perfection: Elevating Construction Standards with Uncompromising Quality and Unparalleled Expertise"
+      />
+
       <Footer />
     </>
   )
