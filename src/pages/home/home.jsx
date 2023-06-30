4695e06562f32de6fe9adebc5fe4e8ed0684e7ac
tharuAnish <namoanishtharu@gmail.com>
2023-06-30 21:55:32 +0530

    footer was made responsive , tried to add messenger chat support

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -7,6 +7,8 @@ import Testimony from "../../components/testimony/testimony"
 import Tagline from "../../components/tagline/tagline"
 import HomeProjects from "./homeProjects/homeProjects"
 import Clients from "../../components/clients/clients"
+import ChatSupport from "../../components/chatSupport/chatSupport"
+// import MessengerCustomerChat from "react-messenger-customer-chat"
 
 export default function Home() {
   return (
@@ -22,6 +24,8 @@ export default function Home() {
         tagH4="WE CAN HELP BUILD YOUR PROJECT"
         tagP="TELL US ABOUT YOUR PROJECT"
       /> */}
+      {/* <MessengerCustomerChat pageId="105457115937923" appId="621826160041746" /> */}
+      <ChatSupport />
       <Footer />
     </div>
   )
