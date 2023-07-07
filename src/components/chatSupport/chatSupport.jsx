1f1dd5bbbbc55712ba5979b599f0d7ed48fef2e1
tharuAnish <namoanishtharu@gmail.com>
2023-07-07 23:39:59 +0530

    jokes and quotes added to chat support

--- src/components/chatSupport/chatSupport.jsx
+++ src/components/chatSupport/chatSupport.jsx
@@ -3,6 +3,52 @@ import "./chatSupport.css"
 import { FiUser } from "react-icons/fi"
 import { Link } from "react-router-dom"
 
+//quots
+const quotes = [
+  "“An optimist will tell you the glass is half-full; the pessimist, half-empty; and the engineer will tell you the glass is twice the size it needs to be.”",
+  "“We're civil engineers, which means we're experts at turning 'oops' into 'aha!' and 'what the heck?' into 'look what we built!”",
+  "“We civil engineers are like magicians; we transform raw materials into awe-inspiring structures, all while hoping no one asks how much it cost.”",
+  "“Every great company started with a vision, fueled by determination, and supported by a team united in purpose.”",
+  "“Being an engineer is like being a magician, except instead of pulling rabbits out of hats, we pull solutions out of thin air... with a lot of calculations and coffee.”",
+  "“Believe in yourself, for you hold within you the power to achieve greatness. Embrace your strengths, conquer your fears, and let your passion guide you on the path to success.”",
+  "“Success is not defined by the applause of others or the accumulation of wealth. True success lies in the impact we make, the lives we touch, and the positive change we bring to the world. Leave a legacy that resonates long after you are gone.”",
+  "“Your potential knows no bounds. Dare to dream big, set audacious goals, and take bold action towards their realization. Believe in yourself, trust in your abilities, and let perseverance be the fuel that propels you forward.”",
+]
+const getRandomQuote = () => {
+  const randomIndex = Math.floor(Math.random() * quotes.length)
+  return quotes[randomIndex]
+}
+
+//jokes
+const jokes = [
+  "What did the traffic light say to the car? Don't look! I'm about to change.",
+  "How did the civil engineer propose to their partner? They got down on one knee and said, Will you be my foundation for life?",
+  'A teacher asked her students to use the word "beans" in a sentence. "My father grows beans," said one girl. "My mother cooks beans," said a boy. A third student spoke up, "We are all human beans.',
+  "What did the ocean say to the beach? Nothing, it just waved.",
+  "What do you call a group of civil engineers at a party? A concrete mixer!",
+  "What did the left eye say to the right eye? Between you and me, something smells.",
+  "Why did the construction company start offering yoga classes to its employees? Because they wanted to help them find balance between steel beams and inner peace!",
+  "What did the big flower say to the little flower? Hi, bud!",
+  "Why did the construction company's picnic get canceled? Because they couldn't find a good foundation for the potato salad!",
+  "What did the big chimney say to the little chimney? You're too young to smoke.",
+  "What did the construction company say when they accidentally built a door in the middle of a wall? We're just creating more opportunities for entry and exit!",
+  "What did the big hand say to the little hand? Got a minute?",
+  "I was going to tell you a joke about boxing but I forgot the punch line.",
+  "What did the rug say to the floor? Don't move, I've got you covered.",
+  'A child asked his father, "How were people born?" So his father said, "Adam and Eve made babies, then their babies became adults and made babies, and so on." The child then went to his mother, asked her the same question and she told him, "We were monkeys then we evolved to become like we are now." The child ran back to his father and said, "You lied to me!" His father replied, "No, your mom was talking about her side of the family."',
+  "What month is the shortest of the year? May, it only has three letters.",
+  "What did the stamp say to the envelope? Stick with me and we'll go places.",
+  'Teacher: "Kids, what does the chicken give you?" Student: "Meat!" Teacher: "Very good! Now what does the pig give you?" Student: "Bacon!" Teacher: "Great! And what does the fat cow give you?" Student: "Homework!',
+  "What did the tie say to the hat? You go on ahead. I'll hang around for a while.",
+  "What did the tree say to the wind? Leaf me alone.",
+  "My friend thinks he is smart. He told me an onion is the only food that makes you cry, so I threw a coconut at his face.",
+  "What did the wall say to the ceiling? I'll meet you at the corner.",
+]
+const getRandomJoke = () => {
+  const randomIndex = Math.floor(Math.random() * jokes.length)
+  return jokes[randomIndex]
+}
+
 const handleUserResponse = (userText, handleBotMessage) => {
   // Convert userText to lowercase for case-insensitive matching
   const lowercaseUserText = userText.toLowerCase()
@@ -25,7 +71,10 @@ const handleUserResponse = (userText, handleBotMessage) => {
           <li>Give me contact details.</li>
           <li>Tell me about projects.</li>
           <li>What are the services I can get?</li>
+          <li>Operating hours.</li>
           <li>Tell me about yourself.</li>
+          <li>Yes, I can tell some jokes too.</li>
+          <li>Or, you can just ask for a quote.</li>
         </>
       ),
     })
@@ -139,6 +188,17 @@ const handleUserResponse = (userText, handleBotMessage) => {
       author: "bot",
       text: `You're welcome! If you have any more questions in the future, don't hesitate to reach out. Have a great day!`,
     })
+  } else if (lowercaseUserText.includes("operating")) {
+    handleBotMessage({
+      author: "bot",
+      text: (
+        <>
+          Our office is open from 9:00 AM to 5:00 PM, Monday through Saturday.
+          <br />
+          Sunday: Closed
+        </>
+      ),
+    })
   } else if (lowercaseUserText.includes("yourself")) {
     handleBotMessage({
       author: "bot",
@@ -151,7 +211,7 @@ const handleUserResponse = (userText, handleBotMessage) => {
             target="blank"
             rel="noopener noreferrer"
           >
-            Anish Tharu
+            Er.Anish Tharu
           </Link>{" "}
           to provide information about NG-CivilArchitects. They are a renowned
           architectural firm known for innovative designs, sustainability, and
@@ -184,6 +244,18 @@ const handleUserResponse = (userText, handleBotMessage) => {
         </>
       ),
     })
+  } else if (lowercaseUserText.includes("joke")) {
+    const randomJoke = getRandomJoke()
+    handleBotMessage({
+      author: "bot",
+      text: randomJoke,
+    })
+  } else if (lowercaseUserText.includes("quote")) {
+    const randomQuote = getRandomQuote()
+    handleBotMessage({
+      author: "bot",
+      text: randomQuote,
+    })
   } else {
     handleBotMessage({
       author: "bot",
@@ -196,7 +268,10 @@ const handleUserResponse = (userText, handleBotMessage) => {
           <li>Give me contact details.</li>
           <li>Tell me about projects.</li>
           <li>What are the services I can get?</li>
+          <li>Operating hours.</li>
           <li>Tell me about yourself.</li>
+          <li>Yes, I can tell some jokes too.</li>
+          <li>Or, you can just ask a quote.</li>
         </>
       ),
     })
@@ -227,7 +302,10 @@ const ChatWindow = ({ onClose }) => {
           <li>Give me contact details.</li>
           <li>Tell me about projects.</li>
           <li>What are the services I can get?</li>
+          <li>Operating hours.</li>
           <li>Tell me about yourself.</li>
+          <li>Yes, I can tell some jokes too.</li>
+          <li>Or, you can just ask a quote.</li>
         </>
       ),
     },
