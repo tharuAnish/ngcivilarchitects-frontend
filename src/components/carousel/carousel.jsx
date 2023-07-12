adff97138bba152f239ea72484a9e4d3be841ba0
tharuAnish <namoanishtharu@gmail.com>
2023-07-13 03:35:53 +0530

    glass nave added

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -1,12 +1,12 @@
 import React, { useRef } from "react"
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 import { Carousel } from "react-responsive-carousel"
-import "./carousel.css"
+import s from "./carousel.module.css"
 import { Link } from "react-router-dom"
 
 export default function CarouselComponent() {
   return (
-    <div className="carousel-wrapper">
+    <div className={s.carousel_wrapper}>
       <Carousel
         className="carousel"
         showThumbs={false}
@@ -20,28 +20,28 @@ export default function CarouselComponent() {
       >
         <div>
           <img
-            className="carousel-img zoomIn"
+            className={s.carousel_img}
             src="carousel1.webp"
             alt="Slide 1 - A Civil & Architectural Engineering Consultancy"
             loading="lazy"
           />
-          <div className="carousel-text">
-            <p className="carousel-subline">
+          <div className={s.carousel_text}>
+            <p className={s.carousel_subline}>
               A Civil & Architectural Engineering Consultancy
             </p>
-            <h2 className="carousel-h1">WELCOME TO NG</h2>
-            <p className="carousel-para">
+            <h2 className={s.carousel_h1}>WELCOME TO NG</h2>
+            <p className={s.carousel_para}>
               Experienced construction company dedicated to delivering
               exceptional results. We specialize in residential and commercial
               construction, renovations, and remodeling. Contact us to bring
               your vision to life.
             </p>
-            <div className="carouselBtnDiv">
-              <Link to="/services" className="carouselBtnServices">
+            <div className={s.carouselBtnDiv}>
+              <Link to="/services" className={s.carouselBtnServices}>
                 Our Services
               </Link>
 
-              <Link to="/contact" className="carouselBtnContact">
+              <Link to="/contact" className={s.carouselBtnContact}>
                 Contact
               </Link>
             </div>
@@ -49,28 +49,28 @@ export default function CarouselComponent() {
         </div>
         <div>
           <img
-            className="carousel-img "
+            className={s.carousel_img}
             src="../carousel2.webp"
             alt="Slide 2 - A Civil & Architectural Engineering Consultancy"
             loading="lazy"
           />
-          <div className="carousel-text">
-            <p className="carousel-subline">
+          <div className={s.carousel_text}>
+            <p className={s.carousel_subline}>
               A Civil & Architects Engineering Consultancy
             </p>
-            <h2 className="carousel-h1">WELCOME TO NG</h2>
-            <p className="carousel-para">
+            <h2 className={s.carousel_h1}>WELCOME TO NG</h2>
+            <p className={s.carousel_para}>
               Experienced construction company dedicated to delivering
               exceptional results. We specialize in residential and commercial
               construction, renovations, and remodeling. Contact us to bring
               your vision to life.
             </p>
-            <div className="carouselBtnDiv">
-              <Link to="/services" className="carouselBtnServices">
+            <div className={s.carouselBtnDiv}>
+              <Link to="/services" className={s.carouselBtnServices}>
                 Our Services
               </Link>
 
-              <Link to="/contact" className="carouselBtnContact">
+              <Link to="/contact" className={s.carouselBtnContact}>
                 Contact
               </Link>
             </div>
@@ -78,28 +78,28 @@ export default function CarouselComponent() {
         </div>
         <div>
           <img
-            className="carousel-img"
+            className={s.carousel_img}
             src="/carousel3.webp"
             alt="Slide 3 - A Civil & Architectural Engineering Consultancy"
             loading="lazy"
           />
-          <div className="carousel-text">
-            <p className="carousel-subline">
+          <div className={s.carousel_text}>
+            <p className={s.carousel_subline}>
               A Civil & Architects Engineering Consultancy
             </p>
-            <h2 className="carousel-h1">WELCOME TO NG</h2>
-            <p className="carousel-para">
+            <h2 className={s.carousel_h1}>WELCOME TO NG</h2>
+            <p className={s.carousel_para}>
               Experienced construction company dedicated to delivering
               exceptional results. We specialize in residential and commercial
               construction, renovations, and remodeling. Contact us to bring
               your vision to life.
             </p>
-            <div className="carouselBtnDiv">
-              <Link to="/services" className="carouselBtnServices">
+            <div className={s.carouselBtnDiv}>
+              <Link to="/services" className={s.carouselBtnServices}>
                 Our Services
               </Link>
 
-              <Link to="/contact" className="carouselBtnContact">
+              <Link to="/contact" className={s.carouselBtnContact}>
                 Contact
               </Link>
             </div>
